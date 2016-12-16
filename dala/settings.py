a9b650755d0467408d73b804114f01ca440cebde
DiliniHB <dilinihb@gmail.com>
2016-12-16 23:07:53 +0530

    changed db configurations

--- dala/settings.py
+++ dala/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'OPTIONS': {
                 'options': '-c search_path=health,public'
             },
-        'NAME': 'dala_new',
+        'NAME': 'dala',
         'USER': 'postgres',
 
     },
