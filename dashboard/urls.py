22c02a553073f483ed43ec00bb0ddc021af237b7
dilini-himali <thilinihb@gmail.com>
2016-12-16 18:13:28 +0530

    added charts and reports

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,6 @@ app_name = 'dashboard'
 urlpatterns = [
 
 
-    url(r'^$', views.index,name='index'),
+    url(r'^index$', views.index,name='index'),
 
 ]
--- reports/views.py
+++ reports/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):   
-    return render(request, 'reports/index.html')
+def index_report(request):   
+    return render(request, 'reports/index_report.html')
+
+def index_chart(request):   
+    return render(request, 'reports/index_chart.html')
+
+def chart_tabel8(request):   
+    return render(request, 'reports/chart_tabel8.html')
+
+def chart_tabel9(request):   
+    return render(request, 'reports/chart_tabel9.html')
+
+def chart_tabel10(request):   
+    return render(request, 'reports/chart_tabel10.html')
 
 def health_summery_damageloss_dis_report(request):   
     return render(request, 'reports/health_summery_damageloss_dis_report.html')
